2198c83d760f4a4f1b5d5b3b0d71cd589147a0ed
Clint Kunz <clintkunz@gmail.com>
2019-06-11 17:18:44 -0600

    missing breaks in switch, MUL works

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -30,6 +30,7 @@ void cpu_load(struct cpu *cpu, char *filename)
   int address = 0;
   FILE *fp;
   char line[1024];
+  printf("%s\n", filename);
   fp = fopen(filename, "r");
 
   if (fp == NULL) {
@@ -47,7 +48,7 @@ void cpu_load(struct cpu *cpu, char *filename)
       continue;
     }
 
-    //printf("%u\n", v); 
+    printf("%u\n", v); 
     cpu->ram[address] = v;
     address++; 
   }
@@ -75,7 +76,8 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
-
+  unsigned int operA;
+  unsigned int operB;
   cpu->pc = 0;
 
   unsigned char ir;
@@ -86,27 +88,42 @@ void cpu_run(struct cpu *cpu)
     ir = cpu->ram[cpu->pc];
     // 2. Figure out how many operands this next instruction requires
     int numOperands = (ir >> 6) + 1;
+    printf("numOper: %d\n", numOperands);
     // 3. Get the appropriate value(s) of the operands following this instruction
-
+    if (numOperands > 1) {
+      operA = cpu->ram[cpu->pc+1];
+    }
+    if (numOperands > 2) {
+      operB = cpu->ram[cpu->pc+2];
+    }
     // 4. switch() over it to decide on a course of action.
     switch(ir) {
     // 5. Do whatever the instruction should do according to the spec.
     // 6. Move the PC to the next instruction.
+      case MUL: 
+        printf("MUL\n");
+        cpu->registers[operA] = cpu->registers[operA] * cpu->registers[operB];
+        cpu->pc+=numOperands;
+        break;
       case LDI:
         printf("LDI\n");
-        cpu->registers[cpu->ram[cpu->pc+1]] = cpu->ram[cpu->pc+2]; 
-        cpu->pc+numOperands;
+        cpu->registers[operA] = operB; 
+        cpu->pc+=numOperands;
+        break;
       case PRN:
         printf("PRN\n");
-        int v = cpu->registers[cpu->ram[cpu->pc+1]];
+        int v = cpu->registers[operA];
         printf("%d\n", v);
-        cpu->pc+numOperands;
+        cpu->pc+=numOperands;
+        break;
       case HLT:
         printf("HLT\n");
         running = 0; 
+        break; 
       default: 
         printf("Unknown instruction %02x at address %02x\n", ir, cpu->pc);
-        cpu->pc++; 
+        cpu->pc++;
+        break;  
     }
  
   }
